0e997813a6e9eba50296165ba1a1a67a6e58d809
BuildTools <unconfigured@null.spigotmc.org>
2023-05-14 19:33:26 +0100

    does stuff

--- src/commands/inventory.rs
+++ src/commands/inventory.rs
@@ -1,10 +1,12 @@
+use std::fmt::format;
+
 use poise::futures_util::future::ok;
 
 use crate::{Context, Error, utils::database::retrieve_database, utils::database::User};
 
 #[poise::command(prefix_command)]
 pub async fn inv(ctx: Context<'_>) -> Result<(), Error> {
-    let id = *ctx.author().id.as_u64();
+    let id = ctx.author().id.as_u64();
 
     let db_guard = ctx.data().file_lock.lock().await;
     let db: std::collections::HashMap<u64, crate::utils::database::User> = retrieve_database(db_guard.as_str());
@@ -12,12 +14,10 @@ pub async fn inv(ctx: Context<'_>) -> Result<(), Error> {
 
     if ! db.contains_key(id){
         ctx.say("you do not have an inventory").await?;
-        Ok(())
+        return Ok(());
     }
 
-    let usr_inventory = db.get(id).unwrap().inventory;
-    ctx.say("```ini
-    {:?}```
-    ",usr_inventory).await?;
+    let usr_inventory = &db.get(id).unwrap().inventory;
+    ctx.say(format!("inventory: {:?}", usr_inventory)).await?;
     Ok(())
 }
